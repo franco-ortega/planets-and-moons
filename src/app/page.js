e40c838290b7bcb5e87d87d9bff450a21cb99bfa
Franco Ortega <francopdx@gmail.com>
2023-07-19 15:54:29 -0700

    add background to top page

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,8 @@
-import Image from 'next/image';
 import styles from './page.module.css';
 
 export default function Home() {
   return (
-    <main>
+    <main className={styles.main}>
       <h2>Welcome</h2>
     </main>
   );
