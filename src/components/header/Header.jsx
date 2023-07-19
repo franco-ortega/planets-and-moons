8bb5ae506d60db170af8ade67353f717e8bab20f
Franco Ortega <francopdx@gmail.com>
2023-07-19 15:47:05 -0700

    add Nav to Header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,14 +1,10 @@
+import Nav from '../nav/Nav';
+
 export default function Header() {
   return (
     <header>
       <h1>Hello Universe</h1>
-      <nav>
-        <ul>
-          <li>Home</li>
-          <li>Planets</li>
-          <li>Moons</li>
-        </ul>
-      </nav>
+      <Nav />
     </header>
   );
 }
