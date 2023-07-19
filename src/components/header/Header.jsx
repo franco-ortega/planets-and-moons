7e3d7367d1ee366d6119bd0ff50c01fe9e2ac0b6
Franco Ortega <francopdx@gmail.com>
2023-07-19 15:42:59 -0700

    add Header component unstyled

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+export default function Header() {
+  return (
+    <header>
+      <h1>Header</h1>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Planets</li>
+          <li>Moons</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
