54ba8f9320939eb0f6494d30dde3830ad2ddd5f2
Franco Ortega <francopdx@gmail.com>
2023-07-19 15:45:07 -0700

    update copy on Header and top page

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import styles from './page.module.css';
 export default function Home() {
   return (
     <main>
-      <h1>Hello Universe</h1>
+      <h2>Welcome</h2>
     </main>
   );
 }
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <header>
-      <h1>Header</h1>
+      <h1>Hello Universe</h1>
       <nav>
         <ul>
           <li>Home</li>
