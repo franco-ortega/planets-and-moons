341e9e3a54c9ad02a99d8dd2e73bb4001e14cf6a
Franco Ortega <francopdx@gmail.com>
2023-07-24 16:51:20 -0700

    add NavLink links to Nav

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,3 +1,5 @@
+import NavLink from '../navlink/NavLink';
+
 export default function Nav() {
   return (
     <nav>
@@ -5,6 +7,9 @@ export default function Nav() {
         <li>Home</li>
         <li>Planets</li>
         <li>Moons</li>
+        <NavLink title={'Home'} path={'/'} />
+        <NavLink title={'Planets'} path={'/planets'} />
+        <NavLink title={'Moons'} path={'/moons'} />
       </ul>
     </nav>
   );
