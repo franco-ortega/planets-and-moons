47c7c9ab8295439bcd3babb5e90fd4b3d52e4b50
Franco Ortega <francopdx@gmail.com>
2023-07-26 12:40:15 -0700

    refactor PlanetList as server component

--- src/components/planetlist/PlanetList.jsx
+++ src/components/planetlist/PlanetList.jsx
@@ -1,21 +1,10 @@
-'use client';
-
-import { useEffect, useState } from 'react';
 import planetData from '../../../data/planets.json';
 import PlanetLink from '../planetlink/PlanetLink';
 
 export default function PlanetList() {
-  const [planets, setPlanets] = useState(planetData || []);
-
-  useEffect(() => {
-    if (planets.length < 1) {
-      setPlanets(planetData);
-    }
-  }, [planets]);
-
   return (
     <ul>
-      {planets.map((planet) => (
+      {planetData.map((planet) => (
         <li key={planet.title}>
           <PlanetLink title={planet.title} />
         </li>
