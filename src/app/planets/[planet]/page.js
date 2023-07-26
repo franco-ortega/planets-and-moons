68beed0e4486d2d60bf5d1bad04be4d63f7a86c3
Franco Ortega <francopdx@gmail.com>
2023-07-25 17:49:13 -0700

    create dynamic page to display individual planets

---
+++ src/app/planets/[planet]/page.js
@@ -0,0 +1,17 @@
+'use client';
+
+import { useParams } from 'next/navigation';
+import planetData from '../../../data/planets';
+
+export default function Planet() {
+  const params = useParams();
+
+  const planet = planetData.find((planet) => planet.path === params.planet);
+
+  return (
+    <div>
+      <p>This is planet {planet.title}.</p>
+      <p>Info: {planet.info}</p>
+    </div>
+  );
+}
