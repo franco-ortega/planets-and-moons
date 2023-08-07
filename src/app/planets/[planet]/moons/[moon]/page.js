e45c98de91caa51b8f11465f9864ea2e41171e08
Franco Ortega <francopdx@gmail.com>
2023-08-07 12:34:34 -0700

    fix moon page to convert titles for path comparison

--- src/app/planets/[planet]/moons/[moon]/page.js
+++ src/app/planets/[planet]/moons/[moon]/page.js
@@ -3,16 +3,21 @@
 import { useParams } from 'next/navigation';
 import planetData from '../../../../../../data/planets';
 import Moon from '@/components/moon/Moon';
+import convertTitleForPath from '@/utils/convertTitleForPath';
 
 export default function MoonPage() {
   const params = useParams();
 
-  const planet = planetData.find((planet) => planet.path === params.planet);
+  const planet = planetData.find(
+    (planet) => params.planet === convertTitleForPath(planet.title)
+  );
   const planetTitle = planet.title;
 
   // maybe later a moon object will be passed to the Moon component
   const moons = planet.moons;
-  const moon = moons.find((moon) => moon.path === params.moon);
+  const moon = moons.find(
+    (moon) => params.moon === convertTitleForPath(moon.title)
+  );
   const moonTitle = moon.title;
 
   return (
