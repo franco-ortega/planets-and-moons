a2126a140407d229a9631de7c70eadcc94bb6fec
Franco Ortega <francopdx@gmail.com>
2023-08-09 12:30:16 -0700

    add link to return to planet page from moon page

--- src/components/moon/Moon.jsx
+++ src/components/moon/Moon.jsx
@@ -1,9 +1,19 @@
+import Link from 'next/link';
+import convertTitleForPath from '@/utils/convertTitleForPath';
 import styles from './Moon.module.css';
 
 export default function Moon({ moonTitle, planetTitle }) {
+  const planetPath = convertTitleForPath(planetTitle);
+
   return (
     <main className={styles.Moon}>
-      {moonTitle} is a moon of {planetTitle}.
+      <p>
+        {moonTitle} is a moon of {planetTitle}.
+      </p>
+      <br />
+      <p>
+        <Link href={`/planets/${planetPath}`}>Go back to {planetTitle}</Link>.
+      </p>
     </main>
   );
 }
