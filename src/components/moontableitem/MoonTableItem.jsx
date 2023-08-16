41c60419b221e74b0688b8c1a8077477a1909dbe
Franco Ortega <francopdx@gmail.com>
2023-08-16 15:33:30 -0700

    add styling to MoonTableItem

--- src/components/moontableitem/MoonTableItem.jsx
+++ src/components/moontableitem/MoonTableItem.jsx
@@ -1,9 +1,10 @@
 import MoonList from '../moonlist/MoonList';
+import styles from './MoonTableItem.module.css';
 
 export default function MoonTableItem({ planet }) {
   return (
-    <li key={planet.id}>
-      {planet.title}:{' '}
+    <li className={styles.MoonTableItem} key={planet.id}>
+      <h3>{planet.title}</h3>
       <MoonList moons={planet.moons} planetTitle={planet.title} />
     </li>
   );
