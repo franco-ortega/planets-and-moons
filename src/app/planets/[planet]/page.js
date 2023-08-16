2b63d40621e1b56cfdc5baa5136b02d99deae4e8
Franco Ortega <francopdx@gmail.com>
2023-08-16 15:38:27 -0700

    remove unused parentPath prop from planets page

--- src/app/planets/[planet]/page.js
+++ src/app/planets/[planet]/page.js
@@ -1,16 +1,15 @@
 'use client';
 
 import Link from 'next/link';
-import { useParams, usePathname } from 'next/navigation';
+import { useParams } from 'next/navigation';
 import planets from '../../../../data/planets';
 import convertTitleForPath from '@/utils/convertTitleForPath';
 import MoonList from '@/components/moonlist/MoonList';
-import styles from './page.module.css';
 import NoMoons from '@/components/nomoons/NoMoons';
+import styles from './page.module.css';
 
 export default function Planet() {
   const params = useParams();
-  const parentPath = usePathname();
 
   const planet = planets.find(
     (planet) => convertTitleForPath(planet.title) === params.planet
@@ -26,7 +25,7 @@ export default function Planet() {
         {moons.length === 0 ? (
           <NoMoons />
         ) : (
-          <MoonList moons={moons} parentPath={parentPath} planetTitle={title} />
+          <MoonList moons={moons} planetTitle={title} />
         )}
       </ul>
       <p className={styles.link}>
