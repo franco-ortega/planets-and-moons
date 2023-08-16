9a1636fea15fa91c8cb7d983064e1e5bb6eb8e9c
Franco Ortega <francopdx@gmail.com>
2023-08-16 14:35:03 -0700

    update MoonList to link from moons page in addition to planet page

--- src/components/moonlist/MoonList.jsx
+++ src/components/moonlist/MoonList.jsx
@@ -2,14 +2,18 @@ import MoonLink from '../moonlink/MoonLink';
 import convertTitleForPath from '@/utils/convertTitleForPath';
 import styles from './MoonList.module.css';
 
-export default function MoonList({ planet }) {
+export default function MoonList({ parentPath, planet }) {
   const planetPath = convertTitleForPath(planet.title);
+  const isMoonsPath = parentPath === '/moons';
 
   return (
     <ul className={styles.MoonList}>
       {planet.moons.map(({ id, title }) => (
         <li key={id}>
-          <MoonLink planetPath={planetPath} moonTitle={title} />
+          <MoonLink
+            planetPath={isMoonsPath ? `planets/${planetPath}` : planetPath}
+            moonTitle={title}
+          />
         </li>
       ))}
     </ul>
--- src/components/moontable/MoonTable.jsx
+++ src/components/moontable/MoonTable.jsx
@@ -1,9 +1,14 @@
+'use client';
+
 import MoonLink from '../moonlink/MoonLink';
 import planets from '../../../data/planets';
 import styles from './MoonTable.module.css';
 import MoonList from '../moonlist/MoonList';
+import { usePathname } from 'next/navigation';
 
 export default function MoonTable() {
+  const parentPath = usePathname();
+
   return (
     <ul className={styles.MoonTable}>
       {planets.map((planet) => (
@@ -14,12 +19,12 @@ export default function MoonTable() {
               <li key={moon.id}>
                 <MoonLink
                   moonTitle={moon.title}
-                  planetPath={planet.title.toLowerCase()}
+                  planetPath={`planets/${planet.title.toLowerCase()}`}
                 />
               </li>
             ))}
           </ul> */}
-          <MoonList planet={planet} />
+          <MoonList parentPath={parentPath} planet={planet} />
         </li>
       ))}
     </ul>
